a46738c6f8ce0b603db6d49e9544f21ad6743cf2
n0fre <kasperblom@gmail.com>
2021-12-02 19:26:33 +0100

    First commit

---
+++ js/mainFunctions.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    console.log('ready');
+    $('.navbar_wrapper').load('/components/navbar.html');    
+    $('.footer_wrapper').load('/components/footer.html');
+    
+});
